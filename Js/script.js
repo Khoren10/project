57e1fa1a4198a94142ae8321b8eeaed4633b378b
Khoren <mxoren@inbox.ru>
2020-08-06 15:14:04 +0400

    Урок 16  функции, стрелочные функции

--- Js/script.js
+++ Js/script.js
@@ -1,56 +1,51 @@
 "use strict";
 
-/* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+// function declaration
+let num = 20;
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num);
+}
+showFirstMessage('Hello world');
 
 
+// function calc(a,b) {
+//     return (a + b);
+// }
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// console.log(calc(4,3));
+// console.log(calc(6,3));
+// console.log(calc(4,7));
 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+function ret() {
+    let num = 50;
 
+    // .....
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+//function expression
+
+const logger = function() {
+    console.log('Hello');
 };
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
-if (personalMovieDB.count <= 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count <= 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
-}
+logger();
 
 
+//Стрелочные функции
 
-console.log(personalMovieDB);
+const calc = (a, b) => {
+    
+    return a + b;
+};
+console.log(calc(4, 2));
+
+ 
