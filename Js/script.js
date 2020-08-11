3f6a0ff743f79f8d4421760e993bea5b1568f327
Khoren <mxoren@inbox.ru>
2020-08-11 17:01:48 +0400

    Урок 22 Передача по ссылке или по значению,spread operator (ES6 - ES9)

--- Js/script.js
+++ Js/script.js
@@ -1,39 +1,93 @@
 'use strict';
 
-const arr = [2, 13, 26, 8, 10];
+// let a = 5,
+//     b = a;
 
-arr.sort(comperNum);
-console.log(arr);
+//     b = b + 5;
+//     console.log(b);
+//     console.log(a);
 
-function comperNum(a, b) {
-   return a - b;
+//     const obj = {
+//        a: 5,
+//        b: 1
+//     };
+//     const copy = obj; //Ссылка
+//     copy.a = 10;
+   //  console.log(copy);
+   //  console.log(obj);
 
+
+function copy(mainObj) {
+   let objCopy = {};
+
+   let key;
+   for (key in mainObj) {
+      objCopy[key] = mainObj[key];
+   }
+   return objCopy;
 }
 
+const numbers = {
+   a: 2,
+   b: 5,
+   c: {
+      x: 1,
+      y: 3
+   }
+};
 
-// arr[99] = 0;
-// console.log(arr.length);
-// console.log(arr);
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+newNumbers.b = 560;
+console.log(newNumbers);
+console.log(numbers);
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+const add = {
+   d: 17,
+   e: 20
+};
 
-// arr.pop();
-//arr.push(15);
-//console.log(arr);
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
 
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// } 
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'ppppppppppppp';
+console.log(newArray);
+console.log(oldArray);
 
+const video = ['youtube','vimeo','rutube'],
+      blogs = ['wordpress','livejournal','blogger'],
+      internet = [...video,...blogs, 'vk', 'facebook'];
+console.log(internet);
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+function log(a, b, c) {
+   console.log(a);
+   console.log(b);
+   console.log(c);
+}
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join("; "));
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a','b'];
+const newArrray = [...array];
+
+newArrray[1] = 'c';
+console.log(newArrray);
+console.log(array);
+
+const q = {
+   one: 1,
+   two: 2
+};
+
+const newObj = {...q};
+console.log(q);
+console.log(newObj);
+
