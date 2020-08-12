3f36c329792d78afda0ae0e93278085d544757b7
Khoren <mxoren@inbox.ru>
2020-08-12 17:50:47 +0400

    Урок 24 Практика,исползуем объекты

--- Js/script.js
+++ Js/script.js
@@ -1,29 +1,102 @@
 'use strict';
+/* Задание на урок:
 
-let str = "some";
-let strObj = new String(str);
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-// console.log(typeof(strObj));
-// console.log(typeof(str));
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-console.dir([1,2,3]);
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-const solider = {
-   health: 400,
-   armor: 100,
-   sayHello: function() {
-      console.log('Hello');
+
+// Код возьмите из предыдущего домашнего задания
+//1)
+
+const personalMovieDB = {
+   count: 0,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+
+   start: function () {
+      personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+      while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+         personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+      }
+   },
+   rememberMyFilms: function () {
+      for (let i = 0; i < 2; i++) {
+         const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+
+         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+         } else {
+            console.log('error');
+            i--;
+         }
+      }
+   },
+   detectPersonalLevel: function () {
+      if (personalMovieDB.count <= 10) {
+         console.log("Просмотрено довольно мало фильмов");
+      } else if (personalMovieDB.count <= 30) {
+         console.log("Вы классический зритель");
+      } else if (personalMovieDB.count > 30) {
+         console.log("Вы киноман");
+      } else {
+         console.log("Произошла ошибка");
+      }
+   },
+   showMyDB: function () {
+      if (personalMovieDB.privat == false) {
+         console.log(personalMovieDB);
+      }
+   },
+   toggleVisibleMyDB: function () {                     
+      if (personalMovieDB.privat) {                     
+         personalMovieDB.privat = false;        // --> Упр 2.
+      } else {
+         personalMovieDB.privat = true;
+      }
+   },
+   writeYourGenres: function () {
+      // Способ 1
+      for (let i = 1; i <= 3; i++) {
+         let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+         
+         if (genre == null || genre === '') {
+            console.log('Вы ввели не те данные');
+            i--;
+         }else{
+            personalMovieDB.genres[i - 1] = genre;
+         }
+      }
+
+      // Способ 2
+      //    for (let i = 1; i < 2; i++) {
+      //    let genre = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+      //    if (genre == null || genre === '') {
+      //       console.log('Вы ввели не те данные');
+      //    //    i--;
+      //    }else{
+      //       personalMovieDB.genres = genre.split(', ');
+      //       personalMovieDB.genres.sort();
+      //    }
+      // }
+
+      personalMovieDB.genres.forEach((item, i) => {
+         console.log(`Любимый жанр ${i + 1} - это ${item}`);    // --> Упр 3
+      });
    }
+
 };
-
-const jonh = Object.create(solider);
-// const jonh = {
-//    health: 100
-// };
-
-//jonh.__proto__ = solider; //устаревшый метод
-
-//Object.setPrototypeOf(jonh, solider);
-
-//console.log(jonh.armor);
-jonh.sayHello(); 
