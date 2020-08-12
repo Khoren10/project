5002d8e0ee5c17eaeacf225ea9fabb7677c1a20e
Khoren <mxoren@inbox.ru>
2020-08-12 13:39:12 +0400

    Урок 23: ООП наследование

--- Js/script.js
+++ Js/script.js
@@ -1,93 +1,29 @@
 'use strict';
 
-// let a = 5,
-//     b = a;
+let str = "some";
+let strObj = new String(str);
 
-//     b = b + 5;
-//     console.log(b);
-//     console.log(a);
+// console.log(typeof(strObj));
+// console.log(typeof(str));
 
-//     const obj = {
-//        a: 5,
-//        b: 1
-//     };
-//     const copy = obj; //Ссылка
-//     copy.a = 10;
-   //  console.log(copy);
-   //  console.log(obj);
+console.dir([1,2,3]);
 
-
-function copy(mainObj) {
-   let objCopy = {};
-
-   let key;
-   for (key in mainObj) {
-      objCopy[key] = mainObj[key];
-   }
-   return objCopy;
-}
-
-const numbers = {
-   a: 2,
-   b: 5,
-   c: {
-      x: 1,
-      y: 3
+const solider = {
+   health: 400,
+   armor: 100,
+   sayHello: function() {
+      console.log('Hello');
    }
 };
 
-const newNumbers = copy(numbers);
-newNumbers.a = 10;
-newNumbers.b = 560;
-console.log(newNumbers);
-console.log(numbers);
+const jonh = Object.create(solider);
+// const jonh = {
+//    health: 100
+// };
 
-const add = {
-   d: 17,
-   e: 20
-};
+//jonh.__proto__ = solider; //устаревшый метод
 
-const clone = Object.assign({}, add);
+//Object.setPrototypeOf(jonh, solider);
 
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-newArray[1] = 'ppppppppppppp';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube','vimeo','rutube'],
-      blogs = ['wordpress','livejournal','blogger'],
-      internet = [...video,...blogs, 'vk', 'facebook'];
-console.log(internet);
-
-function log(a, b, c) {
-   console.log(a);
-   console.log(b);
-   console.log(c);
-}
-
-const num = [2, 5, 7];
-log(...num);
-
-const array = ['a','b'];
-const newArrray = [...array];
-
-newArrray[1] = 'c';
-console.log(newArrray);
-console.log(array);
-
-const q = {
-   one: 1,
-   two: 2
-};
-
-const newObj = {...q};
-console.log(q);
-console.log(newObj);
-
+//console.log(jonh.armor);
+jonh.sayHello(); 
