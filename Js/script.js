eb5afb5c8e68aba183db169d08e6510de40e1b1b
Khoren <mxoren@inbox.ru>
2021-01-31 20:30:16 +0400

    Урок 34 События на мобильных

--- Js/script.js
+++ Js/script.js
@@ -17,102 +17,15 @@ P.S. Здесь есть несколько вариантов решения з
 
 'use strict';
 
-// Возьмите свой код из предыдущей практики
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
 
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
-
-    
-
-    const image = document.querySelectorAll('.promo__adv img'),
-        genre = document.querySelector('.promo__genre'),
-        bg = document.querySelector('.promo__bg'),
-        cinema = document.querySelector('.promo__interactive-list'),
-        addition = document.querySelector('form.add'),
-        input = addition.querySelector('.adding__input'),
-        checkbox = addition.querySelector('[type = "checkbox"]');
+    box.addEventListener('touchstart', (e) => {
         
-
-
-    addition.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        let newFilm = input.value;
-        const favorite = checkbox.checked;
-        
-      
-        if (newFilm) {
-
-            if (newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0, 22)}...`;
-            } 
-            if (favorite == true) {
-                console.log("Добавляем любимый фильм");
-            }
-
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
-
-
-            createMoviesList(movieDB.movies, cinema);
-        }
-
-        event.target.reset();
+        e.preventDefault();
+        console.log('start');
+        console.log(e.touches);
     });
 
-
-    const deleteAdv = (arr) => {
-        arr.forEach(element => {
-            element.remove();
-        });
-    };
-
-    const makeChanges = () => {
-        genre.textContent = 'драма';
-        bg.style.backgroundImage = `url(img/bg.jpg)`;
-    };
-
-    const sortArr = (arr) => {
-        arr.sort();
-    };
-
-       
-    function createMoviesList(films, parent) {
-        parent.innerHTML = "";
-        sortArr(films);
-        
-        films.forEach((film, i) => {
-            parent.innerHTML += `
-                <li class="promo__interactive-item">${i + 1} ${film}
-                   <div class="delete"></div>
-                </li>
-            `;
-        });
-        document.querySelectorAll('.delete').forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                btn.parentElement.remove();
-                movieDB.movies.splice(i, 1);
-
-                createMoviesList(films, parent);
-                
-            });
-        });
-    }
-    
-    
-    deleteAdv(image);
-    makeChanges();
-    createMoviesList(movieDB.movies, cinema);
-   
-
 });
-
