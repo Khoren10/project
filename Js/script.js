82d6e92dc29a9d2194ac0084e8ecbfa57441da27
Khoren <mxoren@inbox.ru>
2021-02-20 20:52:49 +0400

    Урок 41. Создаем таймер обратного отсчета на сайте

--- Js/script.js
+++ Js/script.js
@@ -1,33 +1,95 @@
 'use strict';
 
-const now = new Date('2021-02-13');
-// new Date.parse('2021-02-13');
+window.addEventListener('DOMContentLoaded', () => {
+        //Tabs
+        const tabs = document.querySelectorAll('.tabheader__item'),
+                tabsContent = document.querySelectorAll('.tabcontent'),
+                tabsParent = document.querySelector('.tabheader__items');
 
-//Геттеры 
+        function hideTabContent() {
+                tabsContent.forEach(item => {
+                        item.classList.add('hide');
+                        item.classList.remove('show', 'fade');
+                });
+                tabs.forEach(item => {
+                        item.classList.remove('tabheader__item_active');
+                });
+        }
 
-//  console.log(now);
-//  console.log(now.getFullYear());
-//  console.log(now.getMonth());
-//  console.log(now.getDate());
-//  console.log(now.getDay());
-//  console.log(now.getHours());
-//  console.log(now.getUTCHours());
+        function showTabContent(i = 0) {
+                tabsContent[i].classList.add('show', 'fade');
+                tabsContent[i].classList.remove('hide');
+                tabs[i].classList.add('tabheader__item_active');
+        }
+        hideTabContent();
+        showTabContent();
+        tabsParent.addEventListener('click', (event) => {
+                const target = event.target;
+                if (target && target.classList.contains('tabheader__item')) {
+                        tabs.forEach((item, i) => {
+                                if (target == item) {
+                                        hideTabContent();
+                                        showTabContent(i);
+                                }
+                        });
+                }
+        });
 
-// console.log(now.getTimezoneOffset());
-// console.log(now.getTime());
+        //Timer
 
-//Сеттеры
+        const deadLine = '2021-02-21';
 
-// console.log(now.setHours(40));
-console.log(now);
+        function getTimeRemaining(endTime) {
+                const t = Date.parse(endTime) - Date.parse(new Date()),
+                        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                        hours = Math.floor(t / (1000 * 60 * 60) % 24),
+                        minutes = Math.floor((t / 1000 / 60) % 60),
+                        seconds = Math.floor((t / 1000) % 60);
 
-const start = new Date();
+                return {
+                        'total': t,
+                        'days': days,
+                        'hours': hours,
+                        'minutes': minutes,
+                        'seconds': seconds
+                };
+        }
 
-for(let i = 0; i < 100000; i++) {
-        let some = i ** 1;
-}
+        function getZero(num) {
+                if (num >= 0 && num < 10) {
+                        return `0${num}`;
+                }else {
+                        return num;
+                }
+        }
 
+        function setClock(selector, endTime) {
+                const timer = document.querySelector(selector),
+                        days = timer.querySelector('#days'),
+                        hours = timer.querySelector('#hours'),
+                        minutes = timer.querySelector('#minutes'),
+                        seconds = timer.querySelector('#seconds'),
+                        timeInterval = setInterval(updateClock, 1000);
 
-let end = new Date();
+                updateClock();
 
-alert(`Cikl dolzhen lala za ${end - start} milisekund`);
+                function updateClock() {
+                        const t = getTimeRemaining(endTime);
+
+                        days.innerHTML = getZero(t.days);
+                        hours.innerHTML = getZero(t.hours);
+                        minutes.innerHTML = getZero(t.minutes);
+                        seconds.innerHTML = getZero(t.seconds);
+
+                        if (t.total <= 0) {
+                                clearInterval(timeInterval);
+                        }
+                        if (t.total < 0) {
+                                 timer.innerHTML = `<h3>Акция завершилось</h3>`;
+                                 timer.classList.add('close');
+                        }
+
+                }
+        }
+        setClock('.timer', deadLine);
+});
