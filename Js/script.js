f86e003046165079537561af70a6d31550b89bc6
Khoren <mxoren@inbox.ru>
2021-03-06 15:33:45 +0400

    Урок 45. Функции-конструкторы

--- Js/script.js
+++ Js/script.js
@@ -1,146 +1,25 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-        //Tabs
-        let tabs = document.querySelectorAll('.tabheader__item'),
-                tabsContent = document.querySelectorAll('.tabcontent'),
-                tabsParent = document.querySelector('.tabheader__items');
+function User(name,id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+        this.hello = function() {
+                console.log(`Hello ${this.name}`);
+        };
+}
 
-        function hideTabContent() {
-                tabsContent.forEach(item => {
-                        item.classList.add('hide');
-                        item.classList.remove('show', 'fade');
-                });
-                tabs.forEach(item => {
-                        item.classList.remove('tabheader__item_active');
-                });
-        }
+User.prototype.exit = function() {
+        console.log(`Polzovatel ${this.name} ushel`);
+};
 
+const Ivan = new User('Ivan', 28);
+const Alex = new User('Alex', 20);
 
-        function showTabContent(i = 0) {
-                tabsContent[i].classList.add('show', 'fade');
-                tabsContent[i].classList.remove('hide');
-                tabs[i].classList.add('tabheader__item_active');
-        }
-        hideTabContent();
-        showTabContent();
-        tabsParent.addEventListener('click', (event) => {
-                const target = event.target;
-                if (target && target.classList.contains('tabheader__item')) {
-                        tabs.forEach((item, i) => {
-                                if (target == item) {
-                                        hideTabContent();
-                                        showTabContent(i);
-                                }
-                        });
-                }
-        });
+Ivan.exit();
 
-        //Timer
+Ivan.hello();
+Alex.hello();
 
-        const deadLine = '2021-02-22';
-
-        function getTimeRemaining(endTime) {
-                const t = Date.parse(endTime) - Date.parse(new Date()),
-                        days = Math.floor(t / (1000 * 60 * 60 * 24)),
-                        hours = Math.floor(t / (1000 * 60 * 60) % 24),
-                        minutes = Math.floor((t / 1000 / 60) % 60),
-                        seconds = Math.floor((t / 1000) % 60);
-
-                return {
-                        'total': t,
-                        'days': days,
-                        'hours': hours,
-                        'minutes': minutes,
-                        'seconds': seconds
-                };
-        }
-
-        function getZero(num) {
-                if (num >= 0 && num < 10) {
-                        return `0${num}`;
-                } else {
-                        return num;
-                }
-        }
-
-        function setClock(selector, endTime) {
-                const timer = document.querySelector(selector),
-                        days = timer.querySelector('#days'),
-                        hours = timer.querySelector('#hours'),
-                        minutes = timer.querySelector('#minutes'),
-                        seconds = timer.querySelector('#seconds'),
-                        timeInterval = setInterval(updateClock, 1000);
-
-                updateClock();
-
-                function updateClock() {
-                        const t = getTimeRemaining(endTime);
-
-                        days.innerHTML = getZero(t.days);
-                        hours.innerHTML = getZero(t.hours);
-                        minutes.innerHTML = getZero(t.minutes);
-                        seconds.innerHTML = getZero(t.seconds);
-
-                        if (t.total <= 0) {
-                                clearInterval(timeInterval);
-                        }
-                        if (t.total < 0) {
-                                timer.innerHTML = `<h3>Акция завершилось</h3>`;
-                                timer.classList.add('close');
-                        }
-
-                }
-        }
-        setClock('.timer', deadLine);
-
-        
-        // Modal 
-        let modalTrigger = document.querySelectorAll('[data-modal]'),
-                modal = document.querySelector('.modal'),
-                modalClose = modal.querySelector('.modal__close');
-                
-        function openModal () {
-                modal.classList.add('show');
-                modal.classList.remove('hide');
-                document.body.style.overflow = 'hidden';
-                clearInterval(modalTimerId);
-        }
-
-        modalTrigger.forEach(btn => {
-                btn.addEventListener('click', openModal);
-        });
-
-        function closeModal () {
-                modal.classList.add('hide');
-                modal.classList.remove('show');
-                document.body.style.overflow = '';
-        }
-
-        modalClose.addEventListener('click', closeModal);
-
-        modal.addEventListener('click', (e) => {
-              if (e.target === modal) {
-                closeModal();
-              }
-        });
-
-        document.addEventListener('keydown', (e) => {
-                if(e.code === 'Escape' && modal.classList.contains('show')) {
-                        closeModal();
-                }
-        });
-
-        const modalTimerId = setTimeout(openModal, 5000);
-
-        function showModalByScroll () {
-                if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) 
-                {
-                        openModal();
-                        window.removeEventListener('scroll', showModalByScroll);
-                }
-                
-        }
-
-        window.addEventListener('scroll', showModalByScroll);
-});
+console.log(Ivan);
+console.log(Alex);
