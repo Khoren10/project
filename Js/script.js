4fdeb3f7453ccb305a40dbc4806c63bbc6153af4
Khoren <mxoren@inbox.ru>
2021-03-10 21:53:18 +0400

    Урок 48. Используем классы в реальной работе

--- Js/script.js
+++ Js/script.js
@@ -1,34 +1,203 @@
 'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+        //Tabs
+        let tabs = document.querySelectorAll('.tabheader__item'),
+                tabsContent = document.querySelectorAll('.tabcontent'),
+                tabsParent = document.querySelector('.tabheader__items');
 
-class Rectangle {
-        constructor(height, width) {
-                this.height = height;
-                this.width = width;
+        function hideTabContent() {
+                tabsContent.forEach(item => {
+                        item.classList.add('hide');
+                        item.classList.remove('show', 'fade');
+                });
+                tabs.forEach(item => {
+                        item.classList.remove('tabheader__item_active');
+                });
         }
-        calcArea() {
-                return this.height * this.width;
+
+
+        function showTabContent(i = 0) {
+                tabsContent[i].classList.add('show', 'fade');
+                tabsContent[i].classList.remove('hide');
+                tabs[i].classList.add('tabheader__item_active');
         }
-}
+        hideTabContent();
+        showTabContent();
+        tabsParent.addEventListener('click', (event) => {
+                const target = event.target;
+                if (target && target.classList.contains('tabheader__item')) {
+                        tabs.forEach((item, i) => {
+                                if (target == item) {
+                                        hideTabContent();
+                                        showTabContent(i);
+                                }
+                        });
+                }
+        });
 
-class ColoredRectangleWithText extends Rectangle {
-        constructor(height,width,text,bgColor) {
-                super(height,width);
-                this.text = text;
-                this.bgColor = bgColor;
+        //Timer
+
+        const deadLine = '2021-02-22';
+
+        function getTimeRemaining(endTime) {
+                const t = Date.parse(endTime) - Date.parse(new Date()),
+                        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                        hours = Math.floor(t / (1000 * 60 * 60) % 24),
+                        minutes = Math.floor((t / 1000 / 60) % 60),
+                        seconds = Math.floor((t / 1000) % 60);
+
+                return {
+                        'total': t,
+                        'days': days,
+                        'hours': hours,
+                        'minutes': minutes,
+                        'seconds': seconds
+                };
         }
-        showMyProps () {
-                console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+
+        function getZero(num) {
+                if (num >= 0 && num < 10) {
+                        return `0${num}`;
+                } else {
+                        return num;
+                }
         }
-}
 
-const div = new ColoredRectangleWithText(2, 4, 'Hello', 'red');
+        function setClock(selector, endTime) {
+                const timer = document.querySelector(selector),
+                        days = timer.querySelector('#days'),
+                        hours = timer.querySelector('#hours'),
+                        minutes = timer.querySelector('#minutes'),
+                        seconds = timer.querySelector('#seconds'),
+                        timeInterval = setInterval(updateClock, 1000);
 
-console.log(div.calcArea());
-div.showMyProps();
+                updateClock();
 
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(2, 100);
+                function updateClock() {
+                        const t = getTimeRemaining(endTime);
 
-// console.log(long.calcArea());
-// console.log(square.calcArea());
+                        days.innerHTML = getZero(t.days);
+                        hours.innerHTML = getZero(t.hours);
+                        minutes.innerHTML = getZero(t.minutes);
+                        seconds.innerHTML = getZero(t.seconds);
+
+                        if (t.total <= 0) {
+                                clearInterval(timeInterval);
+                        }
+                        if (t.total < 0) {
+                                timer.innerHTML = `<h3>Акция завершилось</h3>`;
+                                timer.classList.add('close');
+                        }
+
+                }
+        }
+        setClock('.timer', deadLine);
+
+
+        // Modal 
+        let modalTrigger = document.querySelectorAll('[data-modal]'),
+                modal = document.querySelector('.modal'),
+                modalClose = modal.querySelector('.modal__close');
+
+        function openModal() {
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+                clearInterval(modalTimerId);
+        }
+
+        modalTrigger.forEach(btn => {
+                btn.addEventListener('click', openModal);
+        });
+
+        function closeModal() {
+                modal.classList.add('hide');
+                modal.classList.remove('show');
+                document.body.style.overflow = '';
+        }
+
+        modalClose.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+                if (e.target === modal) {
+                        closeModal();
+                }
+        });
+
+        document.addEventListener('keydown', (e) => {
+                if (e.code === 'Escape' && modal.classList.contains('show')) {
+                        closeModal();
+                }
+        });
+
+        // const modalTimerId = setTimeout(openModal, 5000);
+
+        function showModalByScroll() {
+                if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+                        openModal();
+                        window.removeEventListener('scroll', showModalByScroll);
+                }
+
+        }
+
+        window.addEventListener('scroll', showModalByScroll);
+
+
+        class MenuCard {
+                constructor(src, alt, title, descr, price, parentSelector) {
+                        this.src = src;
+                        this.alt = alt;
+                        this.title = title;
+                        this.descr = descr;
+                        this.price = price;
+                        this.parent = document.querySelector(parentSelector);
+                        this.transfer = 27;
+                        this.changeToUAH(); 
+                }
+                changeToUAH() {
+                        this.price = this.price * this.transfer;
+                }
+                render() {
+                        const element = document.createElement('div');
+                                element.innerHTML = `
+                                <div class="menu__item">
+                                        <img src=${this.src} alt=${this.alt}>
+                                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                                        <div class="menu__item-descr">${this.descr}</div>
+                                        <div class="menu__item-divider"></div>
+                                        <div class="menu__item-price">
+                                                <div class="menu__item-cost">Цена:</div>
+                                                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                                        </div>
+                                </div>
+                        `;
+                        this.parent.append(element);
+                }
+        }
+        new MenuCard(
+                "img/tabs/vegy.jpg",
+                "vegy",
+                'Меню "Фитнес"',
+                'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+                9,
+                '.menu .container'
+        ).render();
+        new MenuCard(
+                "img/tabs/elite.jpg",
+                "elite",
+                'Меню "Премиум"',
+                'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+                27,
+                '.menu .container'
+        ).render();
+        new MenuCard(
+                "img/tabs/post.jpg",
+                "post",
+                'Меню "Постное"',
+                'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+                18,
+                '.menu .container'
+        ).render();
+      
+});
