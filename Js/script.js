abfb9e7ec33b6c19f3eaefc7e82615e9b5b51b6e
Khoren <mxoren@inbox.ru>
2021-03-11 17:18:04 +0400

    Урок 49 Rest оператор и параметры по умолчанию

--- Js/script.js
+++ Js/script.js
@@ -145,23 +145,32 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         class MenuCard {
-                constructor(src, alt, title, descr, price, parentSelector) {
+                constructor(src, alt, title, descr, price, parentSelector, ...classes) {
                         this.src = src;
                         this.alt = alt;
                         this.title = title;
                         this.descr = descr;
                         this.price = price;
+                        this.classes = classes;
                         this.parent = document.querySelector(parentSelector);
                         this.transfer = 27;
-                        this.changeToUAH(); 
+                        this.changeToUAH();
                 }
                 changeToUAH() {
                         this.price = this.price * this.transfer;
                 }
                 render() {
                         const element = document.createElement('div');
-                                element.innerHTML = `
-                                <div class="menu__item">
+                        if (this.classes.length === 0) {
+                                this.element = 'menu__item';
+                                element.classList.add(this.element);
+                        } else {
+                                this.classes.forEach(className => element.classList.add(className));
+                        }
+
+                        this.classes.forEach(className => element.classList.add(className));
+
+                        element.innerHTML = `
                                         <img src=${this.src} alt=${this.alt}>
                                         <h3 class="menu__item-subtitle">${this.title}</h3>
                                         <div class="menu__item-descr">${this.descr}</div>
@@ -170,7 +179,6 @@ window.addEventListener('DOMContentLoaded', () => {
                                                 <div class="menu__item-cost">Цена:</div>
                                                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                                         </div>
-                                </div>
                         `;
                         this.parent.append(element);
                 }
@@ -199,5 +207,5 @@ window.addEventListener('DOMContentLoaded', () => {
                 18,
                 '.menu .container'
         ).render();
-      
+
 });
